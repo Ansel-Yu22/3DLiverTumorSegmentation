31c6347c387dacecaf4665195318c4578d040bbc
YuBo <2933960565@qq.com>
2026-03-21 02:31:57 +0800

    chore: remove unused Desktop package initializer

--- Desktop/__init__.py
+++
@@ -1,4 +0,0 @@
-from Desktop.api_client import ApiClient
-from Desktop.inference import MetricsCalculator, Test_DataSet
-from Desktop.main_window import MainWindow, Ui_MainWindow
-from Desktop.views import AccountDialog, LoginGateDialog
